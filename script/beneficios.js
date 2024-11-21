5b324756f370a471abe56457ef8df7f318739118
Gina-Mariana <ginanlchuquimia@gmail.com>
2024-11-21 18:13:39 -0400

    Add files via upload

---
+++ script/beneficios.js
@@ -0,0 +1,74 @@
+document.getElementById('imagen').addEventListener('click', function() {
+    var imagen = document.getElementById('imagen');
+    
+    if (imagen.classList.contains('moved')) {
+        imagen.classList.remove('moved');
+    } else {
+        imagen.classList.add('moved');
+    }
+});
+document.getElementById('imagen2').addEventListener('click', function() {
+    var imagen2 = document.getElementById('imagen2');
+    
+    if (imagen2.classList.contains('moved')) {
+        imagen2.classList.remove('moved');
+    } else {
+        imagen2.classList.add('moved');
+    }
+});
+document.getElementById('imagen3').addEventListener('click', function() {
+    var imagen3 = document.getElementById('imagen3');
+    
+    if (imagen3.classList.contains('moved')) {
+        imagen3.classList.remove('moved');
+    } else {
+        imagen3.classList.add('moved');
+    }
+});
+document.getElementById('imagen4').addEventListener('click', function() {
+    var imagen4 = document.getElementById('imagen4');
+    
+    if (imagen4.classList.contains('moved')) {
+        imagen4.classList.remove('moved');
+    } else {
+        imagen4.classList.add('moved');
+    }
+});
+document.getElementById('imagen5').addEventListener('click', function() {
+    var imagen5 = document.getElementById('imagen5');
+    
+    if (imagen5.classList.contains('moved')) {
+        imagen5.classList.remove('moved');
+    } else {
+        imagen5.classList.add('moved');
+    }
+});
+document.getElementById('imagen6').addEventListener('click', function() {
+    var imagen6 = document.getElementById('imagen6');
+    
+    if (imagen6.classList.contains('moved')) {
+        imagen6.classList.remove('moved');
+    } else {
+        imagen6.classList.add('moved');
+    }
+});
+document.getElementById('mostrarTextoBtn').addEventListener('click', function() {
+    document.getElementById('textoOculto').classList.toggle('oculto');
+});
+document.getElementById('mostrarTextoBtn2').addEventListener('click', function() {
+    document.getElementById('textoOculto2').classList.toggle('oculto2');
+});
+document.getElementById('mostrarTextoBtn3').addEventListener('click', function() {
+    document.getElementById('textoOculto3').classList.toggle('oculto3');
+});
+document.getElementById('mostrarTextoBtn4').addEventListener('click', function() {
+    document.getElementById('textoOculto4').classList.toggle('oculto4');
+});
+document.getElementById('mostrarTextoBtn5').addEventListener('click', function() {
+    document.getElementById('textoOculto5').classList.toggle('oculto5');
+});
+document.getElementById('mostrarTextoBtn6').addEventListener('click', function() {
+    document.getElementById('textoOculto6').classList.toggle('oculto6');
+});
+
+
---
+++ script/home.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const image = document.querySelector('.animar');
+    const text = document.querySelector('.texto');
+  
+    // Iniciar la animación al cargar la página
+    image.classList.add('animar');
+    text.classList.add('mostrar-texto');
+  });
+  
